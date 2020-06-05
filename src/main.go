3cb25180ed3623e764545e17134cca44aed727c6
xiongyiheng <763285416@qq.com>
2020-06-05 14:50:17 +0800

    revise the whole structure

---
+++ src/main.go
@@ -0,0 +1,12 @@
+package main
+import (
+	"fmt"
+	//"./privacy"
+	//"./encryption"
+	//"./completeness"
+)
+
+func main(){
+	var letters = [10] string {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
+	fmt.Println(letters[1])
+}
---
+++ src/privacy/key.go
@@ -0,0 +1,20 @@
+package privacy
+
+func generateRandomKey() string {
+	var letters = [10] string {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
+	var numbers = [10] int {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	var key =""
+
+	
+
+	return key
+}
+
+func putKeyToBlockChain(userid, key string) bool {
+	return false
+}
+
+func getKeyFromBlockChain(userid string) string {
+	var key = ""
+	return key
+}
